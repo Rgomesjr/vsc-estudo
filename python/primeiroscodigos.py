46f2bb1d13e29d6268d7326c573920a0a08c8a6e
Robson Gomes <rgomesjr18@gmail.com>
2025-07-23 11:48:27 -0300

    primeiros codigos python

--- python/primeiroscodigos.py
+++ python/primeiroscodigos.py
@@ -1,5 +1,30 @@
+print('Hello World')
+print('Meu nome é Robson')
+print('Eu tenho' + ' 28 anos')
+
+
+print('Meu nome é Robson')
+print('eu tenho 28 anos de idade')
+print('meu esporte favorito é o futebol')
+print('nas horas vagas jogo CS.')
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+esporte = input('Digite seu esporte: ')
+jogo = input('Digite seu jogo: ')
+
+print('Seu nome é ' + nome + ',')
+print('você tem ' + idade + ' de idade.')
+print('Seu esporte favorito é o(a) ' +esporte)
+print('e nas horas vagas voce gosta de jogar ' +jogo + '.')
+
+print(nome.upper())
+print(idade.lower())
+print(esporte.upper())
+print(jogo.lower())
+
 p, q, r = 10, 20 ,30
 print(p, q, r)
 
-
-
+var= "James Bond"
+print(var[2::-1])
